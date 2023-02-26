05989d6588646e0bd558c60e00c040f4e0a146b8
xkochevnikx <xkochevnik@protonmail.com>
2023-02-26 13:39:35 +0300

    test git actions

--- config/build/loaders/buildCssLoaders.ts
+++ config/build/loaders/buildCssLoaders.ts
@@ -12,8 +12,7 @@ export function buildCssLoaders(isDev: boolean) {
             options: {
                modules: {
                   //! это настройки для файлов css module тут в опциях при сборке функция автоматически определяет если в названии файла есть модуль то уже дальше мы пишем классы по заданной схеме если нет то она остаются классическими
-                  auto: (resPath: string) =>
-                     Boolean(resPath.includes('.module.')),
+                  auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                   localIdentName: isDev //! тут по шаблонам генерируем названия классов в файлах (в девсборке подробный путь) а в продакшн короткий
                      ? '[path][name]__[local]--[hash:base64:5]'
                      : '[hash:base64:8]',
--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,8 @@
 import { Theme } from 'app/providers/ThemeProvider';
 import { Story } from '@storybook/react';
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
-   return (
-      <div className={`app ${theme}`}>
-         <StoryComponent />
-      </div>
-   );
-};
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+   <div className={`app ${theme}`}>
+      <StoryComponent />
+   </div>
+);
