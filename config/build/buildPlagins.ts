f3bbf757601b97379a01b8b8c9b5d45973c9aafe
xkochevnikx <xkochevnik@protonmail.com>
2023-02-26 14:14:21 +0300

    fix dev plugins

--- config/build/buildPlagins.ts
+++ config/build/buildPlagins.ts
@@ -8,7 +8,7 @@ export function buildPlagins({
    paths,
    isDev,
 }: IBuildOptions): webpack.WebpackPluginInstance[] {
-   return [
+   const plugins = [
       new HtmlWebpackPlugin({
          template: paths.html,
       }),
@@ -20,9 +20,16 @@ export function buildPlagins({
       new webpack.DefinePlugin({
          __IS_DEV__: JSON.stringify(isDev),
       }),
-      new webpack.HotModuleReplacementPlugin(),
-      new BundleAnalyzerPlugin({
-         openAnalyzer: false,
-      }),
    ];
+
+   if (isDev) {
+      plugins.push(
+         new webpack.HotModuleReplacementPlugin(),
+         new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+         })
+      );
+   }
+
+   return plugins;
 }
