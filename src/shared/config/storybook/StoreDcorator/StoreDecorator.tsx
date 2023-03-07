9ed5b9af119cbff4f299ed231787cf94f198668d
xkochevnikx <xkochevnik@protonmail.com>
2023-03-07 13:23:18 +0300

    fix storedecorator

--- src/shared/config/storybook/StoreDcorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDcorator/StoreDecorator.tsx
@@ -7,14 +7,17 @@ const defaultAsyncReducer: DeepPartial<ReducersMapObject<IStateSchema>> = {
     loginForm: loginReducer,
 };
 
-export const StoreDecorator = (
-    state: DeepPartial<IStateSchema>,
-    asyncReducers: DeepPartial<ReducersMapObject<IStateSchema>>,
-) => (StoryComponent: Story) => (
-    <StoreProvider
-        initialState={state}
-        asyncReducers={{ ...defaultAsyncReducer, ...asyncReducers }}
-    >
-        <StoryComponent />
-    </StoreProvider>
-);
+export const StoreDecorator =
+    (
+        state: DeepPartial<IStateSchema>,
+        asyncReducers?: DeepPartial<ReducersMapObject<IStateSchema>>
+    ) =>
+    (StoryComponent: Story) =>
+        (
+            <StoreProvider
+                initialState={state}
+                asyncReducers={{ ...defaultAsyncReducer, ...asyncReducers }}
+            >
+                <StoryComponent />
+            </StoreProvider>
+        );
