8946f22cf3edb5ce8bd153d8acbe144529d432a6
xkochevnikx <xkochevnik@protonmail.com>
2023-03-27 12:46:57 +0300

    fix approuter

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,19 +1,23 @@
 import { getUserAuthData } from 'entities/User';
-import { memo, Suspense, useMemo } from 'react';
+import { Suspense, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader';
 
-function AppRouter() {
+export function AppRouter() {
     const isAuth = useSelector(getUserAuthData);
 
-    const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
-        if (route.authOnly && !isAuth) {
-            return false;
-        }
-        return true;
-    }), [isAuth]);
+    const routes = useMemo(
+        () =>
+            Object.values(routeConfig).filter((route) => {
+                if (route.authOnly && !isAuth) {
+                    return false;
+                }
+                return true;
+            }),
+        [isAuth]
+    );
 
     return (
         <Suspense fallback={<PageLoader />}>
@@ -22,12 +26,10 @@ function AppRouter() {
                     <Route
                         key={path}
                         path={path}
-                        element={<div className="page-wrapper">{element}</div>}
+                        element={<div className='page-wrapper'>{element}</div>}
                     />
                 ))}
             </Routes>
         </Suspense>
     );
 }
-
-export default memo(AppRouter);
