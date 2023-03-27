643c4485bb4b4f6be1bc4426ed15bbc8ec2db712
xkochevnikx <xkochevnik@protonmail.com>
2023-03-27 12:25:40 +0300

    update profile, add ui select, avatar

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,13 +1,24 @@
-import { Suspense } from 'react';
+import { getUserAuthData } from 'entities/User';
+import { memo, Suspense, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader';
 
 export function AppRouter() {
+    const isAuth = useSelector(getUserAuthData);
+
+    const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
+        if (route.authOnly && !isAuth) {
+            return false;
+        }
+        return true;
+    }), [isAuth]);
+
     return (
         <Suspense fallback={<PageLoader />}>
             <Routes>
-                {Object.values(routeConfig).map(({ element, path }) => (
+                {routes.map(({ element, path }) => (
                     <Route
                         key={path}
                         path={path}
@@ -19,4 +30,4 @@ export function AppRouter() {
     );
 }
 
-export default AppRouter;
+export default memo(AppRouter);
