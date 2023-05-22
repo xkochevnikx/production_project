3eaa6cc89fd12c72a7e819b20b5bd6a4ec79381d
xkochevnikx <xkochevnik@protonmail.com>
2023-05-22 19:27:57 +0300

    fix api.ts add interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,12 +1,21 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/consts/localstorage';
 
 //! это инстанс для работы с url что бы каждый раз не тащить в асинкфанк аксиос и не писать адрес с заголовком. его мы передадим в глобальный стор и будем получать через эекстра аргументы и подставлять в запрос на бэк
 export const $api = axios.create({
-    //! используем глобальную переменную котора доступна благодаря плагину. Если при сборке мы не задали переменную окружения env то берётся дефолтное значение и передаётся в плагин
+    //! используем глобальную переменную которая доступна благодаря плагину. Если при сборке мы не задали переменную окружения env то берётся дефолтное значение и передаётся в плагин
     baseURL: __API__,
     //! заголовки имитируют проверку авторизации пользователя и передают в запросе имя, бэк откажет на сложные запросы без него.
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+    // headers: {
+    //     authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
+    // },
+});
+
+//! interceptor это паттерн который перед любым запросом будет добавлять ключи в хедерс
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization =
+            localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
