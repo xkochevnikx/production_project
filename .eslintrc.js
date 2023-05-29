7c44ea4c8c83d484d8908947700a7162240224ce
xkochevnikx <xkochevnik@protonmail.com>
2023-05-29 10:47:45 +0300

    fix version eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint', 'react-hooks', 'fsd'],
+    plugins: ['react', '@typescript-eslint', 'react-hooks', 'fsd-path'],
     rules: {
         'react/jsx-indent': [2, 4], //! правило отступов. 2 - правило работает. 4 - кол-во отсупов
         'react/jsx-indent-props': [2, 4], //! отступы для кода с пропсами
@@ -45,7 +45,7 @@ module.exports = {
         'react/no-array-index-key': 'off', //! отключаем правило запрещающее использование индекса в качестве ключа. Если у нас массив не изменяемый то индекс использовать можно
         'react/jsx-no-useless-fragment': 'off',
         // 'arrow-body-style': 'off',
-        'fsd/path-checker': 'error',
+        'fsd-path/path-checker': 'error',
     },
     globals: {
         //! что бы не ругался на глобальные переменные
