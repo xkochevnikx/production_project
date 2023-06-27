dd442c665a959db429ee98ed34735be45408821c
xkochevnikx <xkochevnik@protonmail.com>
2023-06-27 13:24:32 +0300

    add Dropdown stories

--- src/shared/UI/Popups/ui/Dropdown/Dropdown.stories.tsx
+++ src/shared/UI/Popups/ui/Dropdown/Dropdown.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Dropdown } from './Dropdown';
 import { Button } from '../../../Button/Button';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 
 export default {
     title: 'shared/Dropdown',
@@ -8,6 +10,21 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        (Story) => (
+            <div
+                style={{
+                    display: 'flex',
+                    width: '100vw',
+                    height: '100vh',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Story />
+            </div>
+        ),
+    ],
 } as ComponentMeta<typeof Dropdown>;
 
 const Template: ComponentStory<typeof Dropdown> = (arg) => (
@@ -16,6 +33,76 @@ const Template: ComponentStory<typeof Dropdown> = (arg) => (
 
 export const Primary = Template.bind({});
 Primary.args = {
+    trigger: <Button>open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+    ],
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+    trigger: <Button>open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+    ],
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const PrimaryOrange = Template.bind({});
+PrimaryOrange.args = {
+    trigger: <Button>open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+    ],
+};
+PrimaryOrange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export const PrimaryBottomRight = Template.bind({});
+PrimaryBottomRight.args = {
+    direction: 'bottom right',
+    trigger: <Button>open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+    ],
+};
+
+export const PrimaryTopRight = Template.bind({});
+PrimaryTopRight.args = {
+    direction: 'top right',
+    trigger: <Button>open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+    ],
+};
+
+export const PrimaryTopLeft = Template.bind({});
+PrimaryTopLeft.args = {
+    direction: 'top left',
     trigger: <Button>open</Button>,
     items: [
         {
--- src/shared/UI/Popups/ui/ListBox/ListBox.stories.tsx
+++ src/shared/UI/Popups/ui/ListBox/ListBox.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ListBox } from './ListBox';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 
 export default {
     title: 'shared/ListBox',
@@ -7,15 +9,74 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        (Story) => (
+            <div
+                style={{
+                    display: 'flex',
+                    width: '100vw',
+                    height: '100vh',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Story />
+            </div>
+        ),
+    ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (arg) => <ListBox {...arg} />;
 
+const items = [
+    { value: '123', content: '123', disabled: true },
+    { value: '123', content: '123', disabled: false },
+];
+
 export const Primary = Template.bind({});
 Primary.args = {
     value: 'qwerty',
-    items: [
-        { value: '123', content: '123', disabled: true },
-        { value: '123', content: '123', disabled: false },
-    ],
+    items: items,
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+    value: 'qwerty',
+    items: items,
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Orange = Template.bind({});
+Orange.args = {
+    value: 'qwerty',
+    items: items,
+};
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export const PrimaryTopRight = Template.bind({});
+PrimaryTopRight.args = {
+    direction: 'top right',
+    value: 'qwerty',
+    items: items,
+};
+
+export const PrimaryBottomLeft = Template.bind({});
+PrimaryBottomLeft.args = {
+    direction: 'bottom left',
+    value: 'qwerty',
+    items: items,
+};
+
+export const PrimaryBottomRight = Template.bind({});
+PrimaryBottomRight.args = {
+    direction: 'bottom right',
+    value: 'qwerty',
+    items: items,
+};
+
+export const PrimaryDisabled = Template.bind({});
+PrimaryDisabled.args = {
+    readonly: true,
+    value: 'qwerty',
+    items: items,
 };
