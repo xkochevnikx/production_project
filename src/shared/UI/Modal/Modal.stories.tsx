9f8a251938963b60604291feda4f95777103e312
xkochevnikx <xkochevnik@protonmail.com>
2023-06-27 13:29:42 +0300

    add Modal stories

--- src/shared/UI/Modal/Modal.stories.tsx
+++ src/shared/UI/Modal/Modal.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Modal } from './Modal';
+import { Button } from '../Button/Button';
 
 export default {
     title: 'shared/Modal',
@@ -16,6 +17,19 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     isOpen: true,
+    children:
+        'Не следует, однако, забывать, что консультация с широким активом позволяет оценить значение новых принципов формирования материально-технической и кадровой базы.',
+};
+
+export const PrimaryChildrenNode = Template.bind({});
+PrimaryChildrenNode.args = {
+    isOpen: true,
+    children: <Button>кусь</Button>,
+};
+
+export const PrimaryIsOpenFalse = Template.bind({});
+PrimaryIsOpenFalse.args = {
+    isOpen: false,
     children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
 };
 
@@ -25,3 +39,10 @@ Dark.args = {
     children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Orange = Template.bind({});
+Orange.args = {
+    isOpen: true,
+    children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+};
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
