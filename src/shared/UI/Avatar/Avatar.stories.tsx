827395178dc2af069f7aa5f26788e1ef12bfa7f8
xkochevnikx <xkochevnik@protonmail.com>
2023-06-27 14:50:58 +0300

    add Avatar tests and stories

--- src/shared/UI/Avatar/Avatar.stories.tsx
+++ src/shared/UI/Avatar/Avatar.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Krochik from './крош.jpg';
-import { Avatar } from './Avatar';
+import { Avatar } from '../../assets/icons/крош.jpg';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 
 export default {
     title: 'shared/Avatar',
@@ -17,3 +19,16 @@ Primary.args = {
     size: 150,
     src: Krochik,
 };
+export const Dark = Template.bind({});
+Dark.args = {
+    size: 150,
+    src: Krochik,
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Orange = Template.bind({});
+Orange.args = {
+    size: 150,
+    src: Krochik,
+};
+Orange.decorators = [ThemeDecorator(Theme.ORANGE)];
---
+++ src/shared/UI/Avatar/Avatar.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { Avatar } from './Avatar';
+
+// тестирую компонент на предмет навершивания атрибута и класса
+describe('avatar', () => {
+    test('render avatar', () => {
+        render(<Avatar alt='кусь' />);
+        expect(screen.getByAltText('кусь')).toBeInTheDocument();
+        screen.debug();
+    });
+    test('add class', () => {
+        render(<Avatar alt='кусь' className='Krochik' />);
+        expect(screen.getByAltText('кусь')).toHaveClass('Krochik');
+    });
+});
