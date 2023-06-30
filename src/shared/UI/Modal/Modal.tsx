837868a0856fd59b26c9c8763920937b33695806
xkochevnikx <xkochevnik@protonmail.com>
2023-06-30 14:54:27 +0300

    add useModal()

--- src/shared/UI/Modal/Modal.tsx
+++ src/shared/UI/Modal/Modal.tsx
@@ -1,15 +1,9 @@
-import {
-    MutableRefObject,
-    ReactNode,
-    useCallback,
-    useEffect,
-    useRef,
-    useState,
-} from 'react';
+import { ReactNode } from 'react';
 import { classNames, Mode } from 'shared/lib/classNames/classNames';
 import { Portal } from '../Portal/Portal';
 import cls from './Modal.module.scss';
 import { Overlay } from '../Overlay/Overlay';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 
 interface ModalProps {
     className?: string;
@@ -18,47 +12,15 @@ interface ModalProps {
     onClose?: () => void;
 }
 
-// todo - компонент модального окна принимает состояние функцию закрытия и ноду на отрисовку. модалка изначально имеет ряд стилей, зафиксирована на весь экран, прозрачна и скрыта z index -1. после получения состояния об открытии на неё навештвается дополнительный класс который переопределяет эти стили и портал монтирует этот компонент в боди поверх всего. В себе модалка содержит переиспользуемый компонет подложку с затемненным фоном имеющий индекс меньше контентного поэтому контент лежит поверх подложки которая растянута на всю видимую область экрана. так же на подложку навешиваю функцю закрытия
+// todo - компонент модального окна принимает состояние функцию закрытия и ноду на отрисовку и передаёт в хук. модалка изначально имеет ряд стилей, зафиксирована на весь экран, прозрачна и скрыта z index -1. после получения состояния об открытии на неё навештвается дополнительный класс который переопределяет эти стили и портал монтирует этот компонент в боди поверх всего. В себе модалка содержит переиспользуемый компонет подложку с затемненным фоном имеющий индекс меньше контентного поэтому контент лежит поверх подложки которая растянута на всю видимую область экрана. так же на подложку навешиваю функцю закрытия. СОСТОЯНИЕ, ФУНКЦИЮ ЗАКРЫТИЯ И ЗАДЕРЖКУ ДЛЯ АНИМАЦИИ ПРИНИМАЕТ СНАЧАЛА ХУК КОТОРЫЙ СОДЕРЖИТ В СЕБЕ ПЕРЕИСПОЛЬЗУЕМУЮ В ДВУХ КОМПОНЕНТАХ ЛОГИКУ И ВОЗВРАЩАЕТ СОСТОЯНИЕ ДЛЯ АНИМАЦИИ ЗАКРЫТИЯ ФУНКЦИЮ ЗАКРЫТИЯ И ТЕКУЩЕЕ СОСТОЯНИЕ.
 export function Modal(props: ModalProps) {
-    const [isClosing, setIsClosing] = useState(false);
+    const { className, children, isOpen, onClose } = props;
 
-    const timerRef = useRef() as MutableRefObject<
-        ReturnType<typeof setTimeout>
-    >;
-
-    const {
-        className, children, isOpen, onClose,
-    } = props;
-
-    // todo - функция хендлер сначала проверяет передана ли сверху функция закрытия и изменяет локальное состояние которое нужно для навешивания доп класса для анимации по закрытию окна. в момент навешивания класса окно уменьшается, вызывается функция закрытия которая изменяет прокинутый сверху флаг и модалка исчезает возвращая свои дефлтные стили скрывающие её. таймаут сохранён в реф что бы в эффекте при размонтировании очистить его.
-    const closeHandler = useCallback(() => {
-        if (onClose) {
-            setIsClosing(true);
-            timerRef.current = setTimeout(() => {
-                onClose();
-                setIsClosing(false);
-            }, 300);
-        }
-    }, [onClose]);
-
-    const onKeyDown = useCallback(
-        (e: KeyboardEvent) => {
-            if (e.key === 'Escape') {
-                closeHandler();
-            }
-        },
-        [closeHandler],
-    );
-    //! если модалка открыта, флаг передан, на глобальное окно навешиваем слушатель событий который будет запускать функцию которая проверяем если нажали escape то вызываем функцию закрытия модалки и в конце очищаем таймер и очищаем событие
-    useEffect(() => {
-        if (isOpen) {
-            window.addEventListener('keydown', onKeyDown);
-        }
-        return () => {
-            clearInterval(timerRef.current);
-            window.removeEventListener('keydown', onKeyDown);
-        };
-    }, [isOpen, onKeyDown]);
+    const { closeHandler, isClosing, isMounted } = useModal({
+        isOpen,
+        onClose,
+        animationDelay: 300,
+    });
 
     //! поскольку события всплывают с родительсткого элемента, на дочернем отменяем вызов функции закрытия модалки
     // function onContentClick(e: React.MouseEvent): void {
@@ -66,7 +28,7 @@ export function Modal(props: ModalProps) {
     // }
 
     const mods: Mode = {
-        [cls.opened]: isOpen,
+        [cls.opened]: isMounted,
         [cls.isClosing]: isClosing,
     };
 
---
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -0,0 +1,59 @@
+import {
+    MutableRefObject,
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
+
+interface useModalProps {
+    onClose?: () => void;
+    isOpen?: boolean;
+    animationDelay: number;
+}
+
+export function useModal({ onClose, isOpen, animationDelay }: useModalProps) {
+    const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(isOpen);
+        }
+    }, [isOpen]);
+
+    const timerRef = useRef() as MutableRefObject<
+        ReturnType<typeof setTimeout>
+    >;
+    // todo - функция хендлер сначала проверяет передана ли сверху функция закрытия и изменяет локальное состояние которое нужно для навешивания доп класса для анимации по закрытию окна. в момент навешивания класса окно уменьшается, вызывается функция закрытия которая изменяет прокинутый сверху флаг и модалка исчезает возвращая свои дефлтные стили скрывающие её. таймаут сохранён в реф что бы в эффекте при размонтировании очистить его.
+    const closeHandler = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, animationDelay);
+        }
+    }, [onClose, animationDelay]);
+
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                closeHandler();
+            }
+        },
+        [closeHandler]
+    );
+    //! если модалка открыта, флаг передан, на глобальное окно навешиваем слушатель событий который будет запускать функцию которая проверяем если нажали escape то вызываем функцию закрытия модалки и в конце очищаем таймер и очищаем событие
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            clearInterval(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    return { closeHandler, isClosing, isMounted };
+}
