f9fadf688ff51a994a845396de9443436d7157df
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-07-12 17:51:36 +0300

    fix src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from '@/app/App';
 import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
-import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import { StoreProvider } from '@/app/providers/StoreProviders';
 
 //! импортируем конфиг переводов
 import './shared/config/i18n/i18n';
+import { ThemeProvider } from './shared/lib/context/ThemeContext';
 
 const container = document.getElementById('root');
 if (!container) {
@@ -23,5 +23,5 @@ root.render(
                 </ThemeProvider>
             </ErrorBoundary>
         </StoreProvider>
-    </BrowserRouter>,
+    </BrowserRouter>
 );
