0e5e359bc31848ee47767ee912473ac1b7f8e323
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-08-01 10:29:54 +0300

    fix .browserslistrc mode and buildPlagins

--- config/build/buildPlagins.ts
+++ config/build/buildPlagins.ts
@@ -20,11 +20,7 @@ export function buildPlagins({
         }),
         //! показывает проценты в процессе сборки
         new webpack.ProgressPlugin(),
-        new MiniCssExtractPlagin({
-            //! указываем названия файлов и ниже названия асинхронных чанков компонентов которые обёрнуты в лази или добавляются динамически
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
+
         //! плагин для прокидывания глобальных переменных в приложение что бы они были везде доступны. в качестве значений выступют либо переменные окружения env либо то что идёт вместо них по умолчанию. по умолчпнию тс ничего не знает про тип этой переменной поэтому в глабальных стилях надо этот тп объявить
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
@@ -32,10 +28,7 @@ export function buildPlagins({
             //! эта переменная нужна для того что бы в глобальной области задавать характер сборки. В Конфиге сторибука эта переменная задана как сторибук, и это позволяет задавать условия что бы при рендеринге компонента он например юзеффект не отправлял запрос на сервер для получения данных потому что задаём мы данные сами фейковые.
             __PROJECT__: JSON.stringify(project),
         }),
-        //! плагин для копирования чего то в сборку. в данном случае переводов
-        new CopyPlugin({
-            patterns: [{ from: paths.locales, to: paths.buildLocales }],
-        }),
+
         //! плагин для проверки типов. проверка не влияет на скорость сборки основного кода
         new ForkTsCheckerWebpackPlugin({
             typescript: {
@@ -62,7 +55,29 @@ export function buildPlagins({
             new CircularDependencyPlugin({
                 exclude: /node_modules/,
                 failOnError: true,
-            }),
+            })
+        );
+    }
+
+    if (!isDev) {
+        plugins.push(
+            new MiniCssExtractPlagin({
+                //! указываем названия файлов и ниже названия асинхронных чанков компонентов которые обёрнуты в лази или добавляются динамически
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename:
+                    'css/[name].[contenthash:8].css',
+            })
+        );
+        plugins.push(
+            //! плагин для копирования чего то в сборку. в данном случае переводов
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: paths.locales,
+                        to: paths.buildLocales,
+                    },
+                ],
+            })
         );
     }
 
